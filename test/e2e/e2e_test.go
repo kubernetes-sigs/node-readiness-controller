cbaec620bcd9283bb5a86bba8ffbbfb68aff239e
vitorfloriano <107767584+vitorfloriano@users.noreply.github.com>
2026-07-10 13:32:09 -0300

    cleanup(e2e): reorder uninstall CRD to avoid getting stuck

--- test/e2e/e2e_test.go
+++ test/e2e/e2e_test.go
@@ -103,12 +103,12 @@ var _ = Describe("Manager", Ordered, func() {
 			}
 		}
 
-		By("undeploying the controller-manager")
-		cmd = exec.Command("make", "undeploy")
-		_, _ = utils.Run(cmd)
-
 		By("uninstalling CRDs")
 		cmd = exec.Command("make", "uninstall")
+		_, _ = utils.Run(cmd)
+
+		By("undeploying the controller-manager")
+		cmd = exec.Command("make", "undeploy")
 		_, _ = utils.Run(cmd)
 
 		By("removing manager namespace")
